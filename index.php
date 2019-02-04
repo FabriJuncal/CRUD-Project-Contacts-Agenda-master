b22e05e7e16fe80e898a0457e437281e5e712939
Fabricio Juncal <juncalfabri@gmail.com>
2019-02-04 03:51:47 -0300

    Terminamos de dar estilo al listado de contactos

--- index.php
+++ index.php
@@ -40,5 +40,79 @@
     </form>
 </div> <!-- bg-amarillo contenedor sombra -->
 
+<div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+        <h2>Contactos</h2>
+
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar Contactos...">
+
+        <p class="total-contactos"><span>2</span> Contactos</p>
+
+        <div class="contenedor-tabla">
+            <table id="listado-contactos" class="listado-contactos">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Empresa</th>
+                        <th>Teléfono</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Fabricio</td>
+                        <td>Udemy</td>
+                        <td>3764154511</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+
+                            <!-- "data-id" es un atributo personalisado, es una nueva caracteristica de HTML, agragando data-[nombre], creamos el atributo personalisado -->
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Fabricio</td>
+                        <td>Udemy</td>
+                        <td>3764154511</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+
+                            <!-- "data-id" es un atributo personalisado, es una nueva caracteristica de HTML, agragando data-[nombre], creamos el atributo personalisado -->
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Fabricio</td>
+                        <td>Udemy</td>
+                        <td>3764154511</td>
+                        <td>
+                            <a href="#" class="btn-editar btn">
+                                <i class="fas fa-pen-square"></i>
+                            </a>
+
+                            <!-- "data-id" es un atributo personalisado, es una nueva caracteristica de HTML, agragando data-[nombre], creamos el atributo personalisado -->
+                            <button data-id="1" type="button" class="btn-borrar btn">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            
+            </table>
+        </div> <!-- contenedor-tabla -->
+    </div> <!-- .contenedor-contactos -->
+</div> <!-- bg-blanco contenedor sombra contactos -->
+
 
 <?php include 'inc/layout/footer.php'; ?>
