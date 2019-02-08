2f68befc8dae6b3a0d6a7ed8de0b88b39bf0b78f
Fabricio Juncal <juncalfabri@gmail.com>
2019-02-08 20:11:24 -0300

    Creamos una Notficación cuando el Formulario esta vacio

--- js/app.js
+++ js/app.js
@@ -17,8 +17,41 @@ function leerFormulario(e){
           telefono = document.querySelector('#telefono').value;
     
     if(nombre === '' || empresa === '' || telefono === ''){
-        console.log('Los campos estan vacio')
+        // 2 parametros: texto y clase
+        mostrarNotificacion('Todos los Campos son Obligatorios', 'error'); 
     }else{
         console.log('Tiene algo')
     }
+}
+
+//Notificacion en pantalla
+function mostrarNotificacion(mensaje,clase){
+    // Creamos el nodo
+    const notificacion = document.createElement('div');
+    // Le insertamos una clase al nodo
+    notificacion.classList.add(clase,'notificacion','sombra');
+    // Le insertamos texto al nodo
+    notificacion.textContent = mensaje;
+
+    // Insertamos el nodo creado antes del Legend
+    formularioContactos.insertBefore(notificacion, document.querySelector('form legend'));
+
+    // Ejecutamos la funcion en un intervalo de tiempo de 100milisegundos
+    setTimeout(() => {
+        // Agregamos una clase al nodo para Mostrarlo
+        notificacion.classList.add('visible');
+
+        // Ejecutamos la funcion en un intervalo de tiempo de 3 segundos
+        setTimeout(() => {
+            // Quitamos la clase del nodo
+            notificacion.classList.remove('visible');
+            
+            setTimeout(() => {
+                // Eliminamos el nodo
+                notificacion.remove();
+            },500)
+
+        }, 3000)
+
+    },100)
 }
