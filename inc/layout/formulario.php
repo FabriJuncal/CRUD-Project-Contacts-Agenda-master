fcae278fe65883711d625d2b12e281e54dad70c3
Fabricio Juncal <juncalfabri@gmail.com>
2019-02-08 17:05:53 -0300

    Creando la pagina para editar

---
+++ inc/layout/formulario.php
@@ -0,0 +1,24 @@
+<div class="campos">
+
+            <!-- Nombre -->
+            <div class="campo">
+                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Nombre Contacto" id="nombre">
+            </div><!-- campo -->
+
+            <!-- Empresa -->
+            <div class="campo">
+                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="Nombre Empresa" id="empresa">
+            </div><!-- campo -->
+
+            <!-- Telefono -->
+            <div class="campo">
+                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
+                <label for="telefono">Teléfono:</label>
+                <input type="tel" placeholder="Teléfono Contacto" id="telefono">
+            </div><!-- campo -->
+            
+</div><!-- campos -->
--- index.php
+++ index.php
@@ -8,30 +8,7 @@
     <form id="contacto" action="#">
         <legend>Añada un contacto <span>Todos los campos son obligatorios</span></legend>
 
-        <div class="campos">
-
-            <!-- Nombre -->
-            <div class="campo">
-                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
-                <label for="nombre">Nombre:</label>
-                <input type="text" placeholder="Nombre Contacto" id="nombre">
-            </div><!-- campo -->
-
-            <!-- Empresa -->
-            <div class="campo">
-                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
-                <label for="empresa">Empresa:</label>
-                <input type="text" placeholder="Nombre Empresa" id="empresa">
-            </div><!-- campo -->
-
-            <!-- Telefono -->
-            <div class="campo">
-                <!-- Lo que hace el atributo "for", es que si le damos Click al label, va a seleccionar el elemento con el Id con el mismo nombre -->
-                <label for="telefono">Teléfono:</label>
-                <input type="tel" placeholder="Teléfono Contacto" id="telefono">
-            </div><!-- campo -->
-            
-        </div><!-- campos -->
+        <?php include 'inc/layout/formulario.php'; ?>
 
         <div class="campo enviar">
             <input type="submit" value="Añadir">
