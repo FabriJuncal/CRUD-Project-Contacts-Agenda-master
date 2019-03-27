9e48998d9d1ea0dfdf3c5a16bec489990ee0f69b
Fabricio Juncal <juncalfabri@gmail.com>
2019-03-27 04:05:50 -0300

    Agrego el script para Insertar registros en la BD

--- inc/funciones/bd.php
+++ inc/funciones/bd.php
@@ -14,14 +14,25 @@ define('BD_HOST','localhost');
 define('BD_USUARIO','root');
 define('BD_PASSWORD','');
 define('BD_NOMBRE','agendaphp');
+// define('BD_PORT','3306');
 
 // Los parametros deben ir en esta secuencia: el 5to parametro es opcional (puerto).
 // mysqli(HOST, USUARIO, CONTRASEÑA, NOMBRE DE LA BASE DE DATOS, PUERTO)
+
 $conn = new mysqli(BD_HOST, BD_USUARIO, BD_PASSWORD, BD_NOMBRE);
+
+// Si ocurre un problema de conexion, se imprimira un mensaje del error
+if ($conn->connect_error) {
+    die("CONEXION FALLIDA: " . $conn->connect_error);
+}else{
+    // ping(): Nos muestra dos posibles valores:
+    // 1: Significa que se realizo la conexion con la base de datos.
+    // 0 o (nada): Significa que no se puedo establecer la conexion con la base de datos.
+    // echo $conn->ping();
+
+}
+
  
-// ping(): Nos muestra dos posibles valores:
-// 1: Significa que se realizo la conexion con la base de datos.
-// 0 o (nada): Significa que no se puedo establecer la conexion con la base de datos.
-echo $conn->ping();
+
 
 
--- inc/modelos/modelo-contactos.php
+++ inc/modelos/modelo-contactos.php
@@ -1,4 +1,56 @@
 <?php
 
+// INSERTAMOS un registro
+if($_POST['accion']== 'crear'){
+
+    // Hacemos la conexion a la base de datos
+    require_once('../funciones/bd.php');
+
+    // Validamos las entradas agregandole un FILTRO DE SANEAMIENTO a cada una
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+    $empresa = filter_var($_POST['empresa'], FILTER_SANITIZE_STRING);
+    $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+
+    
+    try {
+        // Validamos los datos insertados en el metodo PREPARED STATEMENTS
+        // Preparamos la plantilla de Insersion
+        $stmt = $conn->prepare("
+
+                                 INSERT INTO contactos (nombre, empresa, telefono) VALUES (?, ?, ?)
+                                
+                               ");
+
+        $stmt->bind_param("sss", $nombre, $empresa, $telefono);
+
+        // Ejecutamos la consulta
+        $stmt->execute();
+
+        if($stmt->affected_rows == 1){
+            $respuesta = array(
+                'respuesta' => 'correcto',
+                'datos' => array(
+                    'nombre' => $nombre,
+                    'empresa' => $empresa,
+                    'telefono' => $telefono,
+                    'id_insertado' => $stmt->insert_id
+                )
+            );
+        }
+        
+        // Cerramos las conexiones
+        $stmt->close();
+        $conn->close();
+
+
+    } catch (Exception $e) { // Le asignamos a $e la Excepcion
+        $respuesta = array(
+            'error' => $e->getMessage()
+        );
+    } 
+    
+}
+
+
 // Con "json_encode" enviamos los datos en formato JSON
-echo json_encode($_POST);
+echo json_encode($respuesta);
