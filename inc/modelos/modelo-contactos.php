ccf8433b2891c4a6ff0e89259cf397fd8d8c3b9e
Fabricio Juncal <juncalfabri@gmail.com>
2019-03-29 12:46:37 -0300

    Script: Borra registro de la base de datos

--- inc/modelos/modelo-contactos.php
+++ inc/modelos/modelo-contactos.php
@@ -61,6 +61,30 @@ if(isset($_POST['accion'])){
 if(isset($_GET['accion'])){
     // ELIMINAMOS un registro
     if($_GET['accion'] == 'borrar'){
-        echo json_encode($_GET);
+
+        // Hacemos la conexion a la base de datos
+        require_once('../funciones/bd.php');
+        
+        $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+        try{
+            $stmt = $conn->prepare("DELETE FROM contactos WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+
+            if($stmt->affected_rows == 1){
+                $respuesta = array(
+                    'respuesta' => 'correcto'
+                );
+            }
+            $stmt->close();
+            $conn->close();
+
+        } catch(Excepcion $e){
+            $respuesta = array(
+                'error' => $e->getMessage()
+            );
+        }
+        echo json_encode($respuesta);
     }
 }
